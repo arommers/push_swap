7e38b2b92e4b3f48b5676f6b3f58ef650e496405
arommers <a3.p.rommers@gmail.com>
2023-03-19 11:21:52 +0100

    changed input for norminette

--- src/input_check.c
+++ src/input_check.c
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   input_check.c                                      :+:    :+:            */
+/*   input.c                                            :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: adri <adri@student.codam.nl>                 +#+                     */
+/*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/03/18 22:41:09 by adri          #+#    #+#                 */
-/*   Updated: 2023/03/18 22:52:22 by adri          ########   odam.nl         */
+/*   Created: 2023/03/19 11:14:39 by arommers      #+#    #+#                 */
+/*   Updated: 2023/03/19 11:17:21 by arommers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-int check_doubles(int argc, char **argv)
+int	check_doubles(int argc, char **argv)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (i < argc)
-    {
-        j = i + 1;
-        while (j < argc)
-        {
-            if (atoi(argv[i]) == atoi(argv[j]))
-                return (1);
-            j++;
-        }
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (atoi(argv[i]) == atoi(argv[j]))
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
