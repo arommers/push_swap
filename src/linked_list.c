f5561d4272b807ee0a7d34a6629dc31a58753fef
Adri Rommers <adri@adris-air.home>
2023-03-26 18:54:29 +0200

    turned calloc into ft_calloc

--- src/linked_list.c
+++ src/linked_list.c
@@ -6,7 +6,7 @@
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/27 15:11:54 by arommers      #+#    #+#                 */
-/*   Updated: 2023/03/23 10:46:39 by arommers      ########   odam.nl         */
+/*   Updated: 2023/03/26 18:54:03 by adri          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ t_stack	*make_new_node(int new_value)
 {
 	t_stack	*new_node;
 
-	new_node = calloc(1, sizeof(t_stack));
+	new_node = ft_calloc(1, sizeof(t_stack));
 	if (!new_node)
 		return (NULL);
 	new_node->value = new_value;
